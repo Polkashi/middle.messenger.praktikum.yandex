877cc428df47e2f29cee424ae3766e85cbec5e0f
Isa <i.agverdiev@mastercr.ru>
2023-03-12 01:59:38 +0300

    add/userSettings page

--- src/pages/userSettings/userSettings.js
+++ src/pages/userSettings/userSettings.js
@@ -1,10 +1,13 @@
 import Handlebars from 'handlebars'
 import html from 'bundle-text:./userSettings.hbs'
 import { links } from '/src/consts/global.js'
+import avatarBg from '/src/images/avatar-bg.jpg'
 
 export const UserSettingsPage = () => {
+	console.log(Handlebars.compile(html)())
 	const context = {
 		links,
+		avatarBg,
 	}
 	return Handlebars.compile(html)(context)
 }
