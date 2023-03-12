23bc1ca25bd9de32413d69106883dd3ccd8adffd
Polkashi <i.agverdiev@yandex.ru>
2023-03-12 14:26:38 +0300

    fix/routing try2

--- server/index.js
+++ server/index.js
@@ -4,12 +4,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'dist')))
-console.log(__dirname)
-
 app.use(express.static(path.join(__dirname, '../dist')))
 
-app.use('/*', (req, res) => {
+app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
