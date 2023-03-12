c3fba02bc6b844cf97fd5113f07051082216bfe3
Polkashi <i.agverdiev@yandex.ru>
2023-03-12 14:18:15 +0300

    fix/routing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ console.log(__dirname)
 
 app.use(express.static(path.join(__dirname, '../dist')))
 
+app.use('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
 app.listen(PORT, function () {
 	console.log(`Example app listening on port ${PORT}!`)
 })
